79cccba4c76242a1aaeb7d720284510709a2d6ea
Paul Otieno <vitalispaul48@live.com>
2023-11-13 13:19:55 +0300

    Add maticon

--- src/app/location-list/location-list.module.ts
+++ src/app/location-list/location-list.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LocationListRoutingModule } from './location-list-routing.module';
-
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    LocationListRoutingModule
-  ]
+  imports: [CommonModule, LocationListRoutingModule, MaterialModule],
 })
-export class LocationListModule { }
+export class LocationListModule {}
