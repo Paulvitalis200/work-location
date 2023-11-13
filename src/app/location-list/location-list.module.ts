81493ac2d4df8a24c5a5c6d1eb3d7aa20b2b4678
Paul Otieno <vitalispaul48@live.com>
2023-11-13 12:29:37 +0300

    add material

---
+++ src/app/location-list/location-list.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LocationListRoutingModule } from './location-list-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LocationListRoutingModule
+  ]
+})
+export class LocationListModule { }
